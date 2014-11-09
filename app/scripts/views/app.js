2dc39120d1bf83caff63fda7e0dc3ca092fc18d3
Daniela Baron <baron.daniela@gmail.com>
2014-11-08 20:19:31 -0500

    view events

--- app/scripts/views/app.js
+++ app/scripts/views/app.js
@@ -15,13 +15,27 @@ define([
               '<a href="#" class="navbar-brand">Weather Watcher</a>',
               '<ul class="nav navbar-nav">',
                 '<li id="nav-dash"><a href="#">Dashboard</a></li>',
-                '<li id="nav-dash"><a href="#">About</a></li>',
+                '<li id="nav-about"><a href="#">About</a></li>',
               '</ul>',
             '</div>',
             '<div id="content"></div>'
           ].join(''),
+
+    events: {
+      'click #nav-dash': 'onNavDash',
+      'click #nav-about': 'onNavAbout'
+    },
+
     initialize: function() {
       this.$el.append(this.html);
+    },
+
+    onNavAbout: function(e) {
+      console.log('About clicked');
+    },
+
+    onNavDash: function(e) {
+      console.log('Dash clicked');
     }
   });
 
