201665f73af37435e57c295259802fc69e0e220e
Daniela Baron <baron.daniela@gmail.com>
2014-11-10 18:47:52 -0500

    more view unit testing

--- app/scripts/test/views/aboutTest.js
+++ app/scripts/test/views/aboutTest.js
@@ -7,12 +7,18 @@ define([
   var run = function() {
 
     test('AppView Render', function() {
+      var $fixture = $( "#qunit-fixture" );
       var aboutView = new AboutView({
         id: 'page-about',
         className: 'page-view'
       });
-      $('#qunit-fixture').append(aboutView.render().el);
-      strictEqual($('#qunit-fixture').text(), 'About Page', 'the rendered contents');
+      var expectedContent = 'About Page';
+
+      $fixture.append(aboutView.render().el);
+
+      strictEqual($fixture.text(), expectedContent, 'view contains expected content');
+      strictEqual($('#qunit-fixture #page-about').length, 1, 'div with id of page-about should have been rendered');
+      strictEqual($('#qunit-fixture .page-view').length, 1, 'div with class of page-view should have been rendered');
     });
 
   };
