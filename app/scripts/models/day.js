980e0f354fd9748e67782621d781e83df8efdea5
Daniela Baron <baron.daniela@gmail.com>
2014-11-13 18:28:35 -0500

    trying to make url fetch work

--- app/scripts/models/day.js
+++ app/scripts/models/day.js
@@ -10,11 +10,7 @@ define([
       'highFahrenheit': null,
       'lowFahrenheit': null,
       'icon_url': ''
-    },
-
-    // Course sample code didn't have this, but I had to put the url here to make it work
-    // hard code to a particular city for now
-    url: 'http://api.wunderground.com/api/b4313c5e996ab1a9/forecast/q/CA/San_Francisco.json'
+    }
 
   });
 
